e3b4f355573f15420becaa483c3d4f18fae29010
antoo69 <antobarry69@gmail.com>
2024-08-12 11:39:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/antoo69/PURVI_MUSIC",
+    "https://github.com/antoo69/PURVI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -67,7 +67,7 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Nenen_degrees")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+8veNR2baDgMzZjY1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BestieVirtual")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
