cb036ca431638113f7943bb20dfc819f2c5e7dc6
antoo69 <antobarry69@gmail.com>
2024-08-15 12:50:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/antoo69/PURVI1",
+    "https://github.com/antoo69/PURVI3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
